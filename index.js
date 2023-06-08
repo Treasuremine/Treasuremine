af0b24c11f1f9c465716ee82ee7dbc4c50628838
Mallika <132191889+Treasuremine@users.noreply.github.com>
2023-06-08 22:01:32 +0530

    Update and rename main1.js to index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-  
-        let heading=document.getElementById("heading");
+//index.js
+const http=require('http');
+const server = http.createServer((req, res)=>{
+  res.writehead( 200,{'content-type': 'text/plain'});
+  res.end('hello World \n');
+});
+
+server.listen( 3000, () =>{
+  console.log('Server running at http://localhost:8080/');
+});
+
+let heading=document.getElementById("heading");
         let button=document.getElementById("button");
         button.addEventListner("click", function()
         {
